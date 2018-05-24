dc8cede560b6f3ecb9b3f151bb0dfb46284b0560
Katie Wallace <katie@localhost.localdomain>
2018-05-23 20:41:56 -0500

    first commit

---
+++ apps/01_hello_world/you_try/hello_world.py
@@ -0,0 +1,11 @@
+print('***************************')
+print('       Hello World')
+print('***************************')
+print('')
+
+igor = input('What is your name? ')
+greeting = 'Nice to meet you ' + igor
+
+print(greeting) 
+
+
